9daae5a162c95168ca9138cde30221468c1abc4c
PasanAbeysekara <69195287+PasanAbeysekara@users.noreply.github.com>
2023-03-25 21:57:26 +0530

    updates

--- ModernDesign/MVVM/View/HomeView.xaml.cs
+++ ModernDesign/MVVM/View/HomeView.xaml.cs
@@ -13,31 +13,16 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace ModernDesign
+namespace ModernDesign.MVVM.View
 {
 	/// <summary>
-	/// Interaction logic for MainWindow.xaml
+	/// Interaction logic for HomeView.xaml
 	/// </summary>
-	public partial class MainWindow : Window
+	public partial class HomeView : UserControl
 	{
-		public MainWindow()
+		public HomeView()
 		{
 			InitializeComponent();
-		}
-
-		private void RadioButton_Checked(object sender, RoutedEventArgs e)
-		{
-
-        }
-
-		private void RadioButton_Checked_1(object sender, RoutedEventArgs e)
-		{
-
-		}
-
-		private void TextBox_TextChanged()
-		{
-
 		}
 	}
 }
---
+++ ModernDesign/MVVM/ViewModel/MainViewModel.cs
@@ -0,0 +1,42 @@
+﻿using ModernDesign.Core;
+using ModernDesign.MVVM.View;
+using System;
+using System.Linq.Expressions;
+
+namespace ModernDesign.MVVM.ViewModel
+{
+	class MainViewModel:ObservableObject
+	{
+		public RelayCommand HomeViewCommand { get; set; }
+		public RelayCommand DiscoveryViewCommand { get; set; }
+		public DiscoveryViewModel DiscoveryVM { get; set; }
+		public HomeViewModel HomeVM { get; set; }
+
+		// temp setter for maintainting current state
+		private object _currentView;
+
+		public object CurrentView
+		{
+			get { return _currentView; }
+			set { _currentView = value;
+				OnPropertyChanged(); 	
+			}
+		}
+
+		public MainViewModel()
+		{
+			HomeVM = new HomeViewModel();
+			DiscoveryVM = new DiscoveryViewModel();
+
+			CurrentView = HomeVM;
+
+			HomeViewCommand = new RelayCommand( o => {
+				CurrentView = HomeVM;
+			});
+
+			DiscoveryViewCommand = new RelayCommand(o => {
+				CurrentView = DiscoveryVM;
+			});
+		}
+	}
+}
